acbe8f98254853aa9556d88a9fb2425a9ccf02a8
sofiia-tesliuk <sofiia.tesliuk@gmail.com>
2021-04-04 01:49:25 +0300

    add argument parser to emulate_network, add starting script;

--- emulate_network.py
+++ emulate_network.py
@@ -1,18 +1,36 @@
 from scapy.all import *
+import time
 import random
+import argparse
 
 
-DESTINATION_IP = "192.168.99.120"
+DEFAULT_NUMBER_OF_PACKETS = 10
+DEFAULT_BREAK_IN_SECONDS = 2
+
+
+def send_packets(destination, number, sleep_seconds):
+    for i in range(number):
+        source = '192.168.{}.{}'.format(random.randrange(1, 255), random.randrange(1, 255))
+        sendp(Ether()/IP(src=source, dst=destination, ttl=(1, 1)), iface="vboxnet0")
+        print(source)
+        time.sleep(sleep_seconds)
 
 
 def main():
-    for i in range(100):
-        source = '192.168.99.{}'.format(random.randrange(1,255))
-        source_port = 1500
-        destination_port = 20000
-        payload = "yada yada yada"  # packet payload
-        spoofed_packet = IP(src=source, dst=DESTINATION_IP) / TCP(sport=source_port, dport=destination_port) / payload
-        send(spoofed_packet)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dst', help='destination ip address')
+    parser.add_argument('-n', action='store', type=int, help='number of packets to be sent')
+    parser.add_argument('-s', action='store', type=int, help='break in seconds between sending packets')
+
+    args = parser.parse_args()
+
+    if args.n is None:
+        args.n = DEFAULT_NUMBER_OF_PACKETS
+
+    if args.s is None:
+        args.s = DEFAULT_BREAK_IN_SECONDS
+
+    send_packets(args.dst, args.n, args.s)
 
 
 if __name__ == "__main__":
