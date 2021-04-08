4dbf2bae8d35f485544b2205112afb761760246d
sofiia-tesliuk <sofiia.tesliuk@gmail.com>
2021-04-09 02:25:23 +0300

    add argument parser and writing data to file;

--- loader.c
+++ loader.c
@@ -18,39 +18,67 @@
 
 typedef unsigned int u32;
 
-void print_ip_info(u32 ip, int count){
+void print_ip_info(FILE *f, u32 ip, int count){
     unsigned char bytes[4];
     bytes[0] = ip & 0xFF;
     bytes[1] = (ip >> 8) & 0xFF;
     bytes[2] = (ip >> 16) & 0xFF;
     bytes[3] = (ip >> 24) & 0xFF;
 
-    printf("\t%d.%d.%d.%d count: %d\n", bytes[0], bytes[1], bytes[2], bytes[3], count);
+    fprintf(f, "\t%d.%d.%d.%d count: %d\n", bytes[0], bytes[1], bytes[2], bytes[3], count);
 }
 
 int main(int argc, char **argv) {
-    const char *file = "bpf_program.o";
+    char *data_file = NULL;
+    char *ifindex_char = NULL;
+    int c, ifindex;
+
+    while ((c = getopt(argc, argv, "f:i:")) != -1){
+        switch (c){
+            case 'i':
+                ifindex_char = optarg;
+                ifindex = atoi(ifindex_char);
+                if (ifindex == 0){
+                    fprintf(stderr, "ERROR: Invalid index of network device: %s.\n", ifindex_char);
+                    return -1;
+                }
+                break;
+            case 'f':
+                data_file = optarg;
+                break;
+        }
+    }
+
+    FILE *df;
+    df = fopen(data_file, "a+");
+    if (!df){
+        fprintf(stderr, "ERROR: Unable to open file: %s.\n", data_file);
+        return -2;
+    }
+
+    const char *bpf_file = "bpf_program.o";
     struct bpf_object *obj;
     int err, prog_fd, map_fd;
 
-    err = bpf_prog_load(file, BPF_PROG_TYPE_XDP, &obj, &prog_fd);
+    err = bpf_prog_load(bpf_file, BPF_PROG_TYPE_XDP, &obj, &prog_fd);
     if (err < 0) {
         fprintf(stderr, "ERROR: "
                         "bpf program load failed (%d): %s\n",
                 -err, strerror(-err));
-        return -1;
+        return -3;
+    }else{
+        printf("err: %d, prog_fd: %d", err, prog_fd); // Noticed that err (that supposed to be fd of bpf program and prog_fd are different.)
     }
 
     map_fd = bpf_object__find_map_fd_by_name(obj, "ip_map");
     printf("Map fd is: %d\n", map_fd);
 
-    int ifindex = 3;
     err = bpf_set_link_xdp_fd(ifindex, prog_fd, XDP_FLAGS_UPDATE_IF_NOEXIST | XDP_FLAGS_SKB_MODE);
     if (err < 0) {
         fprintf(stderr, "ERROR: "
                         "ifindex(%d) link set xdp fd failed (%d): %s\n",
                 ifindex, -err, strerror(-err));
-        return -2;
+        return -4;
     }
 
     printf("The kernel loaded the BPF program\n");
@@ -61,6 +89,7 @@ int main(int argc, char **argv) {
     u32 next_key;
     int value;
     for(int i = 0; i < 100; i++){
+        fprintf(df, "Cycle %d\n", i);
         printf("Cycle %d\n", i);
         key = 42;
         while (bpf_map_get_next_key(map_fd, &key, &next_key) == 0){
@@ -70,7 +99,7 @@ int main(int argc, char **argv) {
                                 "lookup value with key (%d) failed (%d): %s\n",
                         next_key, -err, strerror(-err));
             }
-            print_ip_info(next_key, value);
+            print_ip_info(df, next_key, value);
             key = next_key;
         }
 
